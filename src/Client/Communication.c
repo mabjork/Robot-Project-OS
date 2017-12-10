c30da02cdf8d4774e3f8e4c78e4f3c23339ae853
“Halvor <halvorbk@gmail.com>
2017-12-10 21:28:31 +0100

    Communication

--- src/Client/Communication.c
+++ src/Client/Communication.c
@@ -8,7 +8,7 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
 
-#define SERV_ADDR   "dc:53:60:ad:61:90"  /* Whatever the address of the server is */
+#define SERV_ADDR "00:17:e9:f5:c9:dd" //"dc:53:60:ad:61:90"  /* Whatever the address of the server is */
 #define TEAM_ID 14                       /* Your team ID */
 
 #define MSG_ACK 0
@@ -21,77 +21,92 @@
 #define MSG_OBSTACLE 7
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 
+void robot () {
+  char string[58];
+  char type;
+  int x1, x2, y1, y2;
+  printf ("I'm navigating...\n");
+
+  srand(time(NULL));
+  // Send POSITION messages
+  position()
+
+  mapdone();
 
 
-//ACK messages are used to acknowledge the reception of messages. They are 8-byte long
-void ack(){
-	*((uint16_t *) string) = msgId++; //msg id
-	//int id; //is a 2-byte number identifying the message (kind of like a sequence number). It is used when acknowledging messages
-	int src = TEAM_ID; //team who sent the message
-	int dst;  //team who should receive the message
-	int type = MSG_ACK; //identifies the kind of message that is sent
-	int idack; //ID of the message that is acknowledged
-	int state; //state is a status code. 0 -> OK, 1 -> error. Other status codes may be used for acknowledging custom messages.
-	
-	//Messages sent by the server should not be acknowledged.
+  printf("I'm waiting for the stop message");
+  while(1){
+    //Wait for stop message
+    read_from_server (s, string, 58);
+    type = string[4];
+    if (type ==MSG_STOP){
+      return;
+    }
+  }
 }
 
-//START messages can only be used by the server. One is sent to each team when the game starts. If the robot disconnects and reconnects
-//during the game, another START message will be sent to it right after it connects to the server. They are 8-byte long
 
-//STOP messages are sent by server to every robot when the game ends. They are 5-bytes long
-
-//KICK messages can only be sent by the server. This message is used to advertise that a robot got kicked out of the game. It is sent
-//to every robot in the game. The message is 6-bytes long
-
-
-//POSITION messages must be sent by robots every 2 seconds. This message is used to advertise the expected position of the robot.
-//The message is 9-bytes long:
-void position(){
-	*((uint16_t *) string) = msgId++;
-	//int ID; 
-	int src = TEAM_ID;
-	int dst = 0XFF;
-	int type = MSG_POSITION;
-	int x; //This field is a signed 16-bits little-endian integer.
-	int y; //This field is a signed 16-bits little-endian integer.
-
-}
-//After the entire map has been generated, the robot sends the server each 5x5 cm grid one pixel at a time.
-void mapdata(){
-	*((uint16_t *) string) = msgId++;
-	//int ID; 
-	int src = TEAM_ID;
-	int dst = 0XFF;
-	int type = MSG_MAPDATA;
-	int x; //the x coordinate of the pixel. This field is a signed 16-bits little-endian integer.
-	int y; //the y coordinate of the pixel. This field is a signed 16-bits little-endian integer.
-	int R; //'R', 'G', and 'B' are the red, green, blue values of the pixel respectively, valued from 0 to 255.
-	int G; 
-	int B; 
-
+void write_to_server (struct team *t, const char *buf, size_t size) {
+    write (t->sock, buf, size);
 }
 
-//These messages signal to the server that their map is finished.
-void mapdone(){
-	*((uint16_t *) string) = msgId++;
-	//int ID; 
-	int src = 14;
-	int dst = 0XFF;
-	int type = MSG_MAPDONE;
+
+int s;
+
+uint16_t msgId = 0;
+
+int read_from_server (int sock, char *buffer, size_t maxSize) {
+  int bytes_read = read (sock, buffer, maxSize);
+
+  if (bytes_read <= 0) {
+    fprintf (stderr, "Server unexpectedly closed connection...\n");
+    close (s);
+    exit (EXIT_FAILURE);
+  }
+
+  printf ("[DEBUG] received %d bytes\n", bytes_read);
+
+  return bytes_read;
 }
 
-//OBSTACLE messages must be sent when a robot picks up or drop an obstacle.
-void obstacle(){
-	*((uint16_t *) string) = msgId++;
-	//int ID; 
-	int src = 14;
-	int dst = 0XFF;
-	int type = MSG_OBSTACLEMS;
-	int act; //act is 0 if the robot dropped obstacle or 1 if it picked it up.
-	int x;
-	int y;
 
+int main(int argc, char **argv) {
+  struct sockaddr_rc addr = { 0 };
+  int status;
+
+  /* allocate a socket */
+  s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+  /* set the connection parameters (who to connect to) */
+  addr.rc_family = AF_BLUETOOTH;
+  addr.rc_channel = (uint8_t) 1;
+  str2ba (SERV_ADDR, &addr.rc_bdaddr);
+
+  /* connect to server */
+  status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+  /* if connected */
+  if( status == 0 ) {
+    char string[58];
+
+    /* Wait for START message */
+    read_from_server (s, string, 9);
+    if (string[4] == MSG_START) {
+      printf ("Received start message!\n");
+
+
+    }
+    //call some function to navigate or similar here, like robot(); in robotclient.c
+    close (s);
+
+    sleep (5);
+
+  } else {
+    fprintf (stderr, "Failed to connect to server...\n");
+    sleep (2);
+    exit (EXIT_FAILURE);
+  }
+
+  close(s);
+  return 0;
 }
-
-//possible to add custom messages...
